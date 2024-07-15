9dd7d405129dc76245f87866995c6dd34fe226e8
bex <30572280+JoshPaulie@users.noreply.github.com>
2024-07-15 10:16:07 -0500

    fix: install path

--- Formula/jagex-launcher.rb
+++ Formula/jagex-launcher.rb
@@ -6,8 +6,11 @@ class JagexLauncher < Formula
   version "1.0.0" 
 
   def install
-    system "hdiutil", "attach", "Jagex%20Launcher%20Installer.dmg"
+    dmg_name = "JagexLauncherInstaller.dmg"
+    system "curl", "-o", dmg_name, url
+    system "hdiutil", "attach", dmg_name
     system "cp", "-r", "/Volumes/Jagex\ Launcher\ Installer/Jagex\ Launcher.app", "#{prefix}/Jagex\ Launcher.app"
+    system "hdiutil", "detach", "/Volumes/Jagex\ Launcher\ Installer"
     bin.install_symlink "#{prefix}/Jagex\ Launcher.app/Contents/MacOS/Jagex\ Launcher" => "jagex-launcher"
   end
 
